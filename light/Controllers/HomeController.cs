291ae97737b21721b47abb1f174fc758a3817b41
zahra <zamanizahra>
2022-10-23 10:37:36 +0430

    compleated main page

--- light/Controllers/HomeController.cs
+++ light/Controllers/HomeController.cs
@@ -13,5 +13,20 @@ namespace light.Controllers
         {
             return View();
         }
+        [Route("AdminPage")]
+        public ActionResult AdminPage()
+        {
+            return View();
+        }
+        [Route("Login")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [Route("Register")]
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
